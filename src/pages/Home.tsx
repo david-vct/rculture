e1aab203815929cb086c84df92b582a0f5be4f8f
David Vicente <davidvicento@gmail.com>
2024-04-05 12:33:35 +0200

    feat: add question progress on quiz

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,9 +4,12 @@ export const Home = () => {
 	return (
 		<div className="w-full min-h-screen flex flex-col items-center justify-center pt-8">
 			<div className="flex flex-col items-center w-full sm:w-auto px-4 py-8 sm:px-8 rounded-3xl">
-				<h1 className="text-9xl font-bold">Rculture</h1>
-				<p className="font-bold pb-16">Un jeu de se culturer avec des quiz</p>
-
+				<h1 className="text-7xl sm:text-9xl font-bold">Rculture</h1>
+				<p className="text-lg sm:text-xl font-medium pb-16">
+					Le jeu de se <span className="line-through decoration-primary decoration-2">culturé</span> culturer avec des{" "}
+					<span className="underline decoration-wavy decoration-primary">quiz</span> et des{" "}
+					<span className="underline decoration-wavy decoration-primary">potes</span>
+				</p>
 				<JoinGame />
 			</div>
 		</div>
